dfc308e3c9d5645ad9c8a7c002113fec4ac504b0
Levent Kırımgeri <nomengo667@gmail.com>
2020-08-05 06:00:54 -0800

    Bar Increasing and Decreasing

---
+++ Assets/Scripts/BarManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarManager : MonoBehaviour
+{
+    public Image experienceBar;
+    public Image healthBar;
+    public Image energyBar;
+    public Text experienceText;
+
+    private int level = 0;
+
+    public void GetExperience(float experience)
+    {
+        experienceBar.GetComponent<Image>().fillAmount += experience;
+    }
+
+    public void HealthDecrease(float damage)
+    {
+        healthBar.GetComponent<Image>().fillAmount -= damage;
+    }
+
+    public void EnergyDecrease(float amount)
+    {
+        energyBar.GetComponent<Image>().fillAmount -= amount;
+    }
+
+    private void Update()
+    {
+        if (experienceBar.GetComponent<Image>().fillAmount == 1)
+        {
+            level++;
+            experienceText.text = "LEVEL: " + level.ToString("0");
+            experienceBar.GetComponent<Image>().fillAmount = 0;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PanelTransitions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelTransitions : MonoBehaviour
+{
+    [Header("PanelGameObject")]
+    public GameObject MainDownPan;
+    public GameObject QuestPan;
+    public GameObject CreaturePan;
+    public GameObject ShopPan;
+
+    public void GoToMainPanel()
+    {
+        ShopPan.SetActive(false);
+        QuestPan.SetActive(false);
+        CreaturePan.SetActive(false);
+        MainDownPan.SetActive(true);
+    }
+
+    public void GoToQuestPanel()
+    {
+        MainDownPan.SetActive(false);
+        QuestPan.SetActive(true);
+    }
+
+    public void GoToHuntingPanel()
+    {
+        MainDownPan.SetActive(false);
+        CreaturePan.SetActive(true);
+    }
+
+    public void GoToShopPanel()
+    {
+        MainDownPan.SetActive(false);
+        ShopPan.SetActive(true);
+    }
+}

d7e5365f44657375e083e6f166bbf56bb92fc917
Levent Kırımgeri <nomengo667@gmail.com>
2020-08-06 06:01:32 -0800

    Third Entry

---
+++ Assets/Scripts/CreatureData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureData 
+{
+    public string creatureName;
+    public int creatureLevel;
+    public int rewardForCreature;
+    public float howMuchEnergyNeededForOneTime;
+    public float howMuchHealthNeededForOneTime;
+    public float howMuchExperienceDoesOneGonnaGet;
+}

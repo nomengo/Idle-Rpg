2250b3a771ecc1018c1f81162042cc50aa2d7b8d
Levent Kırımgeri <nomengo667@gmail.com>
2020-08-11 06:00:34 -0800

    Shop/Item/Inventory(Part-2)

--- Assets/Scripts/CreatureDataList.cs
+++ Assets/Scripts/CreatureDataList.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CreatureDataEnter" , menuName = "CreatureDataEditor")]
+[CreateAssetMenu(fileName = "CreatureDataEnter" , menuName = "CreatureDataEditor" , order = 1)]
 public class CreatureDataList  : ScriptableObject
 {
     //This list is contains creature data 
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ItemData
+{
+    public string ItemName;
+    public long ItemPrice;
+    public string ItemDescription;
+}
+

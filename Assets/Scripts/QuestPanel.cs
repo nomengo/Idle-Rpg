b2c2c2add6892d4a0a100c7f037db5cb02eae7e3
Levent Kırımgeri <nomengo667@gmail.com>
2020-08-21 06:00:59 -0800

    Making Quest Panel For Quests(Part2)

--- Assets/Scripts/QuestPanel.cs
+++ Assets/Scripts/QuestPanel.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class QuestPanel : MonoBehaviour
 {
     [SerializeField] private QuestDataList questDatas;
+    [SerializeField] private QuestGoal questGoal;
 
     public Text titleText;
     public Text descriptionText;
@@ -14,17 +15,50 @@ public class QuestPanel : MonoBehaviour
 
     private void Start()
     {
-
+        foreach(var quest in questDatas.quests)
+        {
+            if (quest.isActive)
+            {
+                titleText.text = quest.questTitle;
+                descriptionText.text = quest.questDescription;
+                rewardText.text = quest.money.ToString("0");
+                experienceText.text = quest.experience.ToString("0");
+            }
+        }
     }
 
-    private void QuestQueue()
+    public void TakeQuest()
     {
-        //while (true)
-        //{
-        //    /*Do some stuff here...
+        foreach (var quest in questDatas.quests)
+        {
+            if (quest.isActive)
+            {
+                questGoal.quest.isActive = quest.isActive;
+                questGoal.quest.questTitle = quest.questTitle;
+                questGoal.quest.questDescription = quest.questDescription;
+                questGoal.quest.money = quest.money;
+                questGoal.quest.experience = quest.experience;
+                questGoal.quest.questID = quest.questID;
+            }
+        }
+    }
 
-
-        //}
+    public void SubmitQuest()
+    {
+        if (questGoal.quest != null)
+        {
+            if (questGoal.quest.isDone)
+            {
+                foreach (var quest in questDatas.quests)
+                {
+                    if (quest.isActive)
+                    {
+                        quest.isActive = false;
+                        break;
+                    }
+                }
+            }
+        }
     }
 
 }

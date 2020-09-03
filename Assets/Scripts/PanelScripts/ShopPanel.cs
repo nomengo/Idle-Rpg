625b4c02b83fc99f19aada90cb03a4c8c7e73494
Levent Kırımgeri <nomengo667@gmail.com>
2020-09-03 05:56:08 -0800

    Working On It

--- Assets/Scripts/PanelScripts/ShopPanel.cs
+++ Assets/Scripts/PanelScripts/ShopPanel.cs
@@ -7,9 +7,11 @@ public class ShopPanel : MonoBehaviour
 {
     private Inventory inventory;
     private BarManager barManager;
+
     public GameObject itemPrefab;
     public GameObject itemContainer;
     public ItemDataList itemData;
+
     [SerializeField] private GameObject itemInfoPan;
 
     void Start()
@@ -29,13 +31,15 @@ public class ShopPanel : MonoBehaviour
     private void ItemInfoCall(ItemData obj)
     {
         itemInfoPan.SetActive(true);
-        itemInfoPan.transform.GetChild(0).Find("NameText").GetComponent<Text>().text = obj.itemName.ToString();
-        itemInfoPan.transform.GetChild(1).Find("PriceText").GetComponent<Text>().text = obj.itemPrice.ToString("0");
-        itemInfoPan.transform.GetChild(2).Find("DescriptionText").GetComponent<Text>().text = obj.itemDescription.ToString();
+        itemInfoPan.transform.GetChild(0).GetChild(1).GetChild(0).Find("NameText").GetComponent<Text>().text = obj.itemName.ToString();
+        itemInfoPan.transform.GetChild(0).GetChild(1).GetChild(1).Find("PriceText").GetComponent<Text>().text = obj.itemPrice.ToString("0");
+        itemInfoPan.transform.GetChild(0).GetChild(1).GetChild(2).Find("DescriptionText").GetComponent<Text>().text = obj.itemDescription.ToString();
+        itemInfoPan.transform.GetChild(0).GetChild(1).GetChild(3).Find("ItemDamageText").GetComponent<Text>().text = obj.itemDamage.ToString();
+        itemInfoPan.transform.GetChild(0).GetChild(1).GetChild(4).Find("ItemArmorText").GetComponent<Text>().text = obj.itemArmor.ToString();
         //Do some other stuff with BUY button here
-        itemInfoPan.transform.GetChild(3).GetComponent<Button>().onClick.AddListener(() => BuyItem(obj));
+        itemInfoPan.transform.GetChild(0).GetChild(1).GetChild(5).GetComponent<Button>().onClick.AddListener(() => BuyItem(obj));
         //
-        itemInfoPan.transform.GetChild(4).GetComponent<Button>().onClick.AddListener(BackToShop);
+        itemInfoPan.transform.GetChild(0).GetChild(1).GetChild(6).GetComponent<Button>().onClick.AddListener(BackToShop);
     }
 
     private void BackToShop()
@@ -49,4 +53,5 @@ public class ShopPanel : MonoBehaviour
         inventory.ItemPickUp(itemData);
         itemInfoPan.SetActive(false);
     }
+
 }

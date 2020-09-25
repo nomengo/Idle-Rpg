ab6de096af20495d54e3624f52d368ad68c24e81
Levent Kırımgeri <nomengo667@gmail.com>
2020-09-25 05:57:19 -0800

    Inventory Upgrades

--- Assets/Scripts/PanelScripts/ItemsYouHavePanel.cs
+++ Assets/Scripts/PanelScripts/ItemsYouHavePanel.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ItemsYouHavePanel : MonoBehaviour
 {
     private YourItemsList yourItems;
+    private PanelTransitions panelTransitions;
 
     public GameObject itemHolder;
     public GameObject allItemsContainer;
 
+    [SerializeField] private GameObject inventoryInfoPlace;
+
+
     private void Awake()
     {
         yourItems = FindObjectOfType<YourItemsList>();
+        panelTransitions = FindObjectOfType<PanelTransitions>();
+    }
+
+    private void Start()
+    {
+        inventoryInfoPlace.SetActive(false);
     }
 
     public void ShowItem()
@@ -51,7 +62,34 @@ public class ItemsYouHavePanel : MonoBehaviour
 
     private void InventoryInfoCall(ItemData itemdAtA)
     {
-        //print(itemdAtA.itemDescription);
+        inventoryInfoPlace.transform.position = new Vector3(inventoryInfoPlace.transform.position.x, -500, inventoryInfoPlace.transform.position.z);
+        inventoryInfoPlace.SetActive(true);
+        inventoryInfoPlace.GetComponent<RectTransform>().DOAnchorPosY(0, .5f);
+
+        inventoryInfoPlace.transform.Find("ItemTypeText").GetComponent<Text>().text = "Type: " + itemdAtA.itemType.ToString();
+        inventoryInfoPlace.transform.Find("RemainingUpdateAmountText").GetComponent<Text>().text = "Upgrade Amount: " + itemdAtA.itemUpgradeAmount.ToString();
+        inventoryInfoPlace.transform.Find("ItemDamageText").GetComponent<Text>().text = "Damage: " + itemdAtA.itemDamage.ToString();
+        inventoryInfoPlace.transform.Find("ItemDefenseText").GetComponent<Text>().text = "Defense: " + itemdAtA.itemArmor.ToString();
+
+        inventoryInfoPlace.transform.Find("ReturnButton").GetComponent<Button>().onClick.AddListener(() => inventoryInfoPlace.SetActive(false));
+        inventoryInfoPlace.transform.Find("DiscardButton").GetComponent<Button>().onClick.AddListener(() => DiscardTheItem(itemdAtA));
+
     }
 
+
+    private void DiscardTheItem(ItemData itDat)
+    {
+        foreach (var item in yourItems.Items)
+        {
+            if (item.itemID == itDat.itemID)
+            {
+                yourItems.Items.Remove(itDat);
+                panelTransitions.GoToMainPanel();
+            }
+            else
+            {
+                //Do nothing
+            }
+        }
+    }
 }
